c598c4c8d84a5f2427874e9d08c65a9a4b943687
김태헌 <chorock@gimtaeheon-ui-MacBookAir.local>
2026-03-14 19:38:10 +0900

    week-5 세션 끝난 후 상태

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,34 @@ import List from './components/List/List'
 
 function App() {
 
-  var title = "LikeLion HUFS"
+  var title = "LikeLion HUFS";
+  
+  let boardList = [
+    {
+      id : 1,
+      title : "안녕하세요. 아기사자입니다.",
+      content : "집에 가고 싶어요. 종강주세요.",
+      date : "2026년 3월 1일"
+    },
+    {
+      id : 2,
+      title : "안녕하세요. 운영진입니다.",
+      content : "저도 집에 가고 싶어요. 종강주세요",
+      date : "2026년 3월 2일"
+    },
+    {
+      id : 3,
+      title : "안녕하세요. 대표입니다.",
+      content : "저는 집에 못 가요. 살려주세요.",
+      date : "2026년 3월 3일"
+    },
+    {
+      id : 4,
+      title : "그냥 아무렇게나 써봤습니다.",
+      content : "이렇게 하니 굳이 글의 개수를 몰라도 되는군요.",
+      date : "2026년 3월 3일"
+    }
+  ]
 
   return (
     <>
@@ -11,11 +38,14 @@ function App() {
       <div className='navbar'>
         <span className='navbar-title'>{title} 블로그</span>
       </div>
-      
       {/* 글 부분임 */}
-      <List/>
-      <List/>
-      <List/>
+
+      {
+      boardList.map(function(data,){
+        return (<List title={data.title} content={data.content} date={data.date} key={data.id}/>)
+      })
+      }
+
     </>
   )
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,10 @@
-export default function List() {
+export default function List({title, content, date}) {
 
   return(
       <div className='list'>
-        <h3>안녕하세요. 아기사자입니다.</h3>
-        <p>집에 가고 싶어요. 종강주세요.</p>
-        <p style={{color : 'gray'}}>2026년 1월 1일</p>
+        <h3>{title}</h3>
+        <p>{content}</p>
+        <p style={{color : 'gray'}}>{date}</p>
       </div>
   )
 }

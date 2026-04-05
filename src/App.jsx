d679f57ddf04d3ba52098363b449e6140e5f268d
김태헌 <chorock@gimtaeheon-ui-MacBookAir.local>
2026-04-05 16:55:27 +0900

    week-5 after asignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,23 @@
-import './App.css'
-import List from './components/List/List'
+import "./App.css";
+import List from "./components/List/List";
+import postData from "./constants/postData";
 
 function App() {
-
   var title = "LikeLion HUFS";
-  
-  let boardList = [
-    {
-      id : 1,
-      title : "안녕하세요. 아기사자입니다.",
-      content : "집에 가고 싶어요. 종강주세요.",
-      date : "2026년 3월 1일"
-    },
-    {
-      id : 2,
-      title : "안녕하세요. 운영진입니다.",
-      content : "저도 집에 가고 싶어요. 종강주세요",
-      date : "2026년 3월 2일"
-    },
-    {
-      id : 3,
-      title : "안녕하세요. 대표입니다.",
-      content : "저는 집에 못 가요. 살려주세요.",
-      date : "2026년 3월 3일"
-    },
-    {
-      id : 4,
-      title : "그냥 아무렇게나 써봤습니다.",
-      content : "이렇게 하니 굳이 글의 개수를 몰라도 되는군요.",
-      date : "2026년 3월 3일"
-    }
-  ]
 
   return (
     <>
       {/* 여긴 nav바 부분임 */}
-      <div className='navbar'>
-        <span className='navbar-title'>{title} 블로그</span>
+      <div className="navbar">
+        <span className="navbar-title">{title} 블로그</span>
       </div>
       {/* 글 부분임 */}
 
-      {
-      boardList.map(function(data,){
-        return (<List title={data.title} content={data.content} date={data.date} key={data.id}/>)
-      })
-      }
-
+      {postData.map(function (data) {
+        return <List data={data} key={data.id} />;
+      })}
     </>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,35 @@
-export default function List({title, content, date}) {
+import "./List.css";
 
-  return(
-      <div className='list'>
-        <h3>{title}</h3>
-        <p>{content}</p>
-        <p style={{color : 'gray'}}>{date}</p>
+export default function List({ data }) {
+  return (
+    <div className="list">
+      {/* 작성자 정보 부분 */}
+      <div
+        style={{
+          display: "flex",
+          gap: "5px",
+          alignItems: "center",
+          alignContent: "center",
+        }}
+      >
+        <img className="profile-img" src={`${data.author.profileImg}`} />
+        <div
+          style={{
+            height: "20px",
+            fontSize: "15px",
+            alignContent: "center",
+          }}
+        >
+          {data.author.name} ({data.author.role})
+        </div>
       </div>
-  )
+      <h3>{data.title}</h3>
+      <p>{data.content}</p>
+      {data.tags.map((tag) => {
+        return <span className="tag">#{tag} </span>;
+      })}
+      <p>❤️ {data.likes}</p>
+      <p style={{ color: "gray" }}>{data.createdAt}</p>
+    </div>
+  );
 }
